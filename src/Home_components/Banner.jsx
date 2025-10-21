13f200d1c0e08886bb5704c7a7fbc71852d8e5b1
SDNATH-Git <sd.nath1952@gmail.com>
2025-10-22 00:56:15 +0600

    added a objects-container

--- src/Home_components/Banner.jsx
+++ src/Home_components/Banner.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 import bannerImg1 from '../assets/banner/banner1.png';
 import bannerImg2 from '../assets/banner/banner2.png';
 import bannerImg3 from '../assets/banner/banner3.png';
 import bannerImg4 from '../assets/banner/banner4.png';
-import { Carousel } from 'react-responsive-carousel';
 
 const Banner = () => {
+    const images = [bannerImg1, bannerImg2, bannerImg3, bannerImg4];
+
     return (
         <div className="my-5">
-            <div className="rounded-2xl overflow-hidden h-[200px] md:h-[300px] lg:h-[500px]">
+            <div className="rounded-2xl overflow-hidden  sm:h-[250px] md:h-[350px] lg:h-[500px] bg-white">
                 <Carousel
                     autoPlay
                     infiniteLoop
                     showThumbs={false}
                     showStatus={false}
-                    showIndicators={false}
+                    showIndicators={true}
+                    interval={3500}
+                    transitionTime={800}
                 >
-                    <div>
-                        <img src={bannerImg1} className="w-full h-[200px] md:h-[300px] lg:h-[500px] object-cover" />
-                    </div>
-                    <div>
-                        <img src={bannerImg2} className="w-full h-[200px] md:h-[300px] lg:h-[500px] object-cover" />
-                    </div>
-                    <div>
-                        <img src={bannerImg3} className="w-full h-[200px] md:h-[300px] lg:h-[500px] object-cover" />
-                    </div>
-                    <div>
-                        <img src={bannerImg4} className="w-full h-[200px] md:h-[300px] lg:h-[500px] object-cover" />
-                    </div>
+                    {images.map((img, i) => (
+                        <div key={i}>
+                            <img
+                                src={img}
+                                alt={`banner-${i + 1}`}
+                                className="w-full  sm:h-[250px] md:h-[350px] lg:h-[500px] object-contain md:object-cover rounded-2xl"
+                            />
+                        </div>
+                    ))}
                 </Carousel>
             </div>
         </div>
-
     );
 };
 
